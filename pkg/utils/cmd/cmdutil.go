764dc5c494c12217106c06d20ea3602e8d6ce6cb
Baha Shaaban <baha.shaaban@securekey.com>
2022-11-17 13:06:16 -0500

    refactor: additional command utility functions
    This change adds GetBool, GetInt, GetFloat, GetDuration and GetTLS utility functions
    
    Signed-off-by: Baha Shaaban <baha.shaaban@securekey.com>

--- pkg/utils/cmd/cmdutil.go
+++ pkg/utils/cmd/cmdutil.go
@@ -1,9 +1,6 @@
 /*
-   Copyright SecureKey Technologies Inc.
-
-   This file contains software code that is the intellectual property of SecureKey.
-   SecureKey reserves all rights in the code and you may not use it without
-	 written permission from SecureKey.
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
 */
 
 package cmd
@@ -12,7 +9,9 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -91,4 +90,131 @@ func GetStringArray(cmd *cobra.Command, flagName, envKey string, isOptional bool
 
 	return nil, errors.New("Neither " + flagName + " (command line flag) nor " + envKey +
 		" (environment variable) have been set.")
+}
+
+// GetBool returns values either command line flag or environment variable.
+func GetBool(cmd *cobra.Command, flagName, envKey string, defaultValue, isOptional bool) (bool, error) {
+	str, err := GetUserSetVarFromString(cmd, flagName, envKey, isOptional)
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", flagName, err)
+	}
+
+	if str == "" {
+		return defaultValue, nil
+	}
+
+	value, err := strconv.ParseBool(str)
+	if err != nil {
+		return false, fmt.Errorf("invalid value for %s [%s]: %w", flagName, str, err)
+	}
+
+	return value, nil
+}
+
+// GetDuration returns values either command line flag or environment variable.
+func GetDuration(cmd *cobra.Command, flagName, envKey string,
+	defaultDuration time.Duration, isOptional bool) (time.Duration, error) {
+	timeoutStr, err := GetUserSetVarFromString(cmd, flagName, envKey, isOptional)
+	if err != nil {
+		return -1, err
+	}
+
+	if timeoutStr == "" {
+		return defaultDuration, nil
+	}
+
+	timeout, err := time.ParseDuration(timeoutStr)
+	if err != nil {
+		return -1, fmt.Errorf("invalid value [%s]: %w", timeoutStr, err)
+	}
+
+	return timeout, nil
+}
+
+// GetInt returns values either command line flag or environment variable.
+func GetInt(cmd *cobra.Command, flagName, envKey string, defaultValue int, isOptional bool) (int, error) {
+	str, err := GetUserSetVarFromString(cmd, flagName, envKey, isOptional)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", flagName, err)
+	}
+
+	if str == "" {
+		return defaultValue, nil
+	}
+
+	value, err := strconv.Atoi(str)
+	if err != nil {
+		return 0, fmt.Errorf("invalid value for %s [%s]: %w", flagName, str, err)
+	}
+
+	return value, nil
+}
+
+// GetFloat returns values either command line flag or environment variable.
+func GetFloat(cmd *cobra.Command, flagName, envKey string, defaultValue float64, isOptional bool) (float64, error) {
+	str, err := GetUserSetVarFromString(cmd, flagName, envKey, isOptional)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", flagName, err)
+	}
+
+	if str == "" {
+		return defaultValue, nil
+	}
+
+	value, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid value for %s [%s]: %w", flagName, str, err)
+	}
+
+	return value, nil
+}
+
+// TLSParameters contains TLS parameters retrieved from command arguments.
+type TLSParameters struct {
+	SystemCertPool bool
+	CACerts        []string
+	ServeCertPath  string
+	ServeKeyPath   string
+}
+
+// TLSFields contains TLS command field names and environment variables key.
+type TLSFields struct {
+	SystemCertPoolFlagName string
+	SystemCertPoolEnvKey   string
+	CACertsFlagName        string
+	CACertsEnvKey          string
+	CertificateFlagName    string
+	CertificateLEnvKey     string
+	KeyFlagName            string
+	KeyEnvKey              string
+}
+
+// GetTLS returns values either command line flag or environment variable.
+func GetTLS(cmd *cobra.Command, tlsFields *TLSFields) (*TLSParameters, error) {
+	tlsSystemCertPoolString := GetUserSetOptionalVarFromString(cmd, tlsFields.SystemCertPoolFlagName,
+		tlsFields.SystemCertPoolEnvKey)
+
+	tlsSystemCertPool := false
+
+	if tlsSystemCertPoolString != "" {
+		var err error
+
+		tlsSystemCertPool, err = strconv.ParseBool(tlsSystemCertPoolString)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	tlsCACerts := GetUserSetOptionalVarFromArrayString(cmd, tlsFields.CACertsFlagName, tlsFields.CACertsEnvKey)
+
+	tlsServeCertPath := GetUserSetOptionalVarFromString(cmd, tlsFields.CertificateFlagName, tlsFields.CertificateLEnvKey)
+
+	tlsServeKeyPath := GetUserSetOptionalVarFromString(cmd, tlsFields.KeyFlagName, tlsFields.KeyEnvKey)
+
+	return &TLSParameters{
+		SystemCertPool: tlsSystemCertPool,
+		CACerts:        tlsCACerts,
+		ServeCertPath:  tlsServeCertPath,
+		ServeKeyPath:   tlsServeKeyPath,
+	}, nil
 }
--- pkg/utils/cmd/cmdutil_test.go
+++ pkg/utils/cmd/cmdutil_test.go
@@ -1,15 +1,14 @@
 /*
-   Copyright SecureKey Technologies Inc.
-
-   This file contains software code that is the intellectual property of SecureKey.
-   SecureKey reserves all rights in the code and you may not use it without
-	 written permission from SecureKey.
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
 */
 
 package cmd
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
@@ -120,13 +119,274 @@ func TestGetString(t *testing.T) {
 	err = command.Execute()
 	require.NoError(t, err)
 
-	// test resolution via command line argument - no environment variable set
-	env, err = GetString(command, flagName, "", false)
-	require.NoError(t, err)
-	require.Equal(t, "other", env)
+	t.Run("test resolution via command line argument - no environment variable set", func(t *testing.T) {
+		env, err = GetString(command, flagName, "", false)
+		require.NoError(t, err)
+		require.Equal(t, "other", env)
 
-	env = GetOptionalString(command, flagName, "")
-	require.Equal(t, "other", env)
+		env = GetOptionalString(command, flagName, "")
+		require.Equal(t, "other", env)
+	})
+}
+
+func TestGetBool(t *testing.T) {
+	command := &cobra.Command{
+		Use:   "start",
+		Short: "short usage",
+		Long:  "long usage",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	t.Run("test unset value should use defaultValue", func(t *testing.T) {
+		env, err := GetBool(command, flagName, envKey, false, true)
+		require.NoError(t, err)
+		require.False(t, env)
+	})
+
+	t.Run("test env var is set", func(t *testing.T) {
+		someIntVal := true
+		t.Setenv(envKey, fmt.Sprint(someIntVal))
+
+		// test resolution via environment variable
+		env, err := GetBool(command, flagName, envKey, true, true)
+		require.NoError(t, err)
+		require.Equal(t, someIntVal, env)
+	})
+
+	t.Run("test invalid env var", func(t *testing.T) {
+		t.Setenv(envKey, "not-an-int")
+
+		env, err := GetBool(command, flagName, envKey, true, true)
+		require.Error(t, err)
+		require.Empty(t, env)
+	})
+
+	t.Run("test invalid flag and envKey for mandatory key", func(t *testing.T) {
+		env, err := GetBool(command, "invalidFlag", "invalidEnvKey", true, false)
+		require.Error(t, err)
+		require.Empty(t, env)
+	})
+}
+
+func TestGetDuration(t *testing.T) {
+	command := &cobra.Command{
+		Use:   "start",
+		Short: "short usage",
+		Long:  "long usage",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	defaultDuration := 10 * time.Second
+
+	t.Run("test unset value should use defaultValue", func(t *testing.T) {
+		env, err := GetDuration(command, flagName, envKey, defaultDuration, true)
+		require.NoError(t, err)
+		require.Equal(t, defaultDuration, env)
+	})
+
+	t.Run("test env var is set", func(t *testing.T) {
+		someIntVal := 15 * time.Second
+		t.Setenv(envKey, fmt.Sprint(someIntVal))
+
+		// test resolution via environment variable
+		env, err := GetDuration(command, flagName, envKey, defaultDuration, true)
+		require.NoError(t, err)
+		require.Equal(t, someIntVal, env)
+	})
+
+	t.Run("test invalid env var", func(t *testing.T) {
+		t.Setenv(envKey, "not-an-int")
+
+		env, err := GetDuration(command, flagName, envKey, defaultDuration, true)
+		require.Error(t, err)
+		require.Less(t, env, 0*time.Second)
+	})
+
+	t.Run("test invalid flag and envKey for mandatory key", func(t *testing.T) {
+		env, err := GetDuration(command, "invalidFlag", "invalidEnvKey", defaultDuration, false)
+		require.Error(t, err)
+		require.Less(t, env, 0*time.Second)
+	})
+}
+
+func TestGetInt(t *testing.T) {
+	command := &cobra.Command{
+		Use:   "start",
+		Short: "short usage",
+		Long:  "long usage",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	t.Run("test unset value should use defaultValue", func(t *testing.T) {
+		env, err := GetInt(command, flagName, envKey, 0, true)
+		require.NoError(t, err)
+		require.Equal(t, 0, env)
+	})
+
+	t.Run("test env var is set", func(t *testing.T) {
+		someIntVal := 15
+		t.Setenv(envKey, fmt.Sprint(someIntVal))
+
+		// test resolution via environment variable
+		env, err := GetInt(command, flagName, envKey, 0, true)
+		require.NoError(t, err)
+		require.Equal(t, someIntVal, env)
+	})
+
+	t.Run("test invalid env var", func(t *testing.T) {
+		t.Setenv(envKey, "not-an-int")
+
+		env, err := GetInt(command, flagName, envKey, 0, true)
+		require.Error(t, err)
+		require.Empty(t, env)
+	})
+
+	t.Run("test invalid flag and envKey for mandatory key", func(t *testing.T) {
+		env, err := GetInt(command, "invalidFlag", "invalidEnvKey", 0, false)
+		require.Error(t, err)
+		require.Empty(t, env)
+	})
+}
+
+func TestGetTLS(t *testing.T) {
+	command := &cobra.Command{
+		Use:   "start",
+		Short: "short usage",
+		Long:  "long usage",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	defaultTLArgs := &TLSFields{
+		SystemCertPoolFlagName: "",
+		SystemCertPoolEnvKey:   "",
+		CACertsFlagName:        "",
+		CACertsEnvKey:          "",
+		CertificateFlagName:    "",
+		CertificateLEnvKey:     "",
+		KeyFlagName:            "",
+		KeyEnvKey:              "",
+	}
+
+	emptyTLSParams := &TLSParameters{
+		SystemCertPool: false,
+		CACerts:        []string{},
+		ServeCertPath:  "",
+		ServeKeyPath:   ""}
+
+	t.Run("test unset value should use defaultValue", func(t *testing.T) {
+		env, err := GetTLS(command, defaultTLArgs)
+		require.NoError(t, err)
+		require.Equal(t, emptyTLSParams, env)
+	})
+
+	t.Run("test env var is set", func(t *testing.T) {
+		someTLSKeys := &TLSFields{
+			SystemCertPoolFlagName: "SystemCertPoolFlagName",
+			SystemCertPoolEnvKey:   "SystemCertPoolEnvKey",
+			CACertsFlagName:        "caCertsFlagName",
+			CACertsEnvKey:          "caCertsEnvKey",
+			CertificateFlagName:    "certificateFlagName",
+			CertificateLEnvKey:     "certificateLEnvKey",
+			KeyFlagName:            "keyFlagName",
+			KeyEnvKey:              "keyEnvKey",
+		}
+
+		sysCertPoolFlagName := true
+		t.Setenv(someTLSKeys.SystemCertPoolFlagName, fmt.Sprint(sysCertPoolFlagName))
+		sysCertPoolEnv := true
+		t.Setenv(someTLSKeys.SystemCertPoolEnvKey, fmt.Sprint(sysCertPoolEnv))
+		caCertsFlagName := "c"
+		t.Setenv(someTLSKeys.CACertsFlagName, caCertsFlagName)
+		caCertsEnvKey := "d"
+		t.Setenv(someTLSKeys.CACertsEnvKey, caCertsEnvKey)
+		certificateFlagName := "e"
+		t.Setenv(someTLSKeys.CertificateFlagName, certificateFlagName)
+		certificateLEnvKey := "f"
+		t.Setenv(someTLSKeys.CertificateLEnvKey, certificateLEnvKey)
+		keyFlagName := "h"
+		t.Setenv(someTLSKeys.KeyFlagName, keyFlagName)
+		keyEnvKey := "i"
+		t.Setenv(someTLSKeys.KeyEnvKey, keyEnvKey)
+
+		// test resolution via environment variable
+		env, err := GetTLS(command, someTLSKeys)
+		require.NoError(t, err)
+		require.Equal(t, sysCertPoolEnv, env.SystemCertPool)
+		require.Equal(t, keyEnvKey, env.ServeKeyPath)
+		require.Equal(t, certificateLEnvKey, env.ServeCertPath)
+	})
+
+	t.Run("test invalid env var", func(t *testing.T) {
+		someTLSKeys := &TLSFields{
+			SystemCertPoolFlagName: "SystemCertPoolFlagName",
+			SystemCertPoolEnvKey:   "SystemCertPoolEnvKey",
+			CACertsFlagName:        "CACertsFlagName",
+			CACertsEnvKey:          "CACertsEnvKey",
+			CertificateFlagName:    "CertificateFlagName",
+			CertificateLEnvKey:     "CertificateLEnvKey",
+			KeyFlagName:            "KeyFlagName",
+			KeyEnvKey:              "KeyEnvKey",
+		}
+
+		sysCertPoolFlagName := true
+		t.Setenv(someTLSKeys.SystemCertPoolFlagName, fmt.Sprint(sysCertPoolFlagName))
+		sysCertPoolEnv := "notBool"
+		t.Setenv(someTLSKeys.SystemCertPoolEnvKey, sysCertPoolEnv)
+
+		// test resolution via environment variable
+		env, err := GetTLS(command, someTLSKeys)
+		require.Error(t, err)
+		require.Empty(t, env)
+	})
+}
+
+func TestGetFloat(t *testing.T) {
+	command := &cobra.Command{
+		Use:   "start",
+		Short: "short usage",
+		Long:  "long usage",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	t.Run("test unset value should use defaultValue", func(t *testing.T) {
+		env, err := GetFloat(command, flagName, envKey, 0, true)
+		require.NoError(t, err)
+		require.Equal(t, 0.0, env)
+	})
+
+	t.Run("test env var is set", func(t *testing.T) {
+		someIntVal := 15.0
+		t.Setenv(envKey, fmt.Sprint(someIntVal))
+
+		// test resolution via environment variable
+		env, err := GetFloat(command, flagName, envKey, 0, true)
+		require.NoError(t, err)
+		require.Equal(t, someIntVal, env)
+	})
+
+	t.Run("test invalid env var", func(t *testing.T) {
+		t.Setenv(envKey, "not-an-int")
+
+		env, err := GetFloat(command, flagName, envKey, 0, true)
+		require.Error(t, err)
+		require.Empty(t, env)
+	})
+
+	t.Run("test invalid flag and envKey for mandatory key", func(t *testing.T) {
+		env, err := GetFloat(command, "invalidFlag", "invalidEnvKey", 0, false)
+		require.Error(t, err)
+		require.Empty(t, env)
+	})
 }
 
 func TestGetStringArray(t *testing.T) {
